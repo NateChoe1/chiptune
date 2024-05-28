b547a2a4dc3e7ad73860effb15e821dcacafecd8
Nate Choe <nate@natechoe.dev>
2024-05-28 08:09:22 -0500

    licensing

--- chiptune.go
+++ chiptune.go
@@ -1,3 +1,19 @@
+// chiptune - a chiptune song generator
+// Copyright (C) 2024 Nate Choe
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 package main
 
 import (
@@ -8,6 +24,21 @@ import (
 )
 
 func main() {
+	fmt.Println("chiptune - a chiptune song generator")
+	fmt.Println("Copyright (C) 2024 Nate Choe")
+	fmt.Println("")
+	fmt.Println("This program is free software: you can redistribute it and/or modify")
+	fmt.Println("it under the terms of the GNU General Public License as published by")
+	fmt.Println("the Free Software Foundation, either version 3 of the License, or")
+	fmt.Println("(at your option) any later version.")
+	fmt.Println("")
+	fmt.Println("This program is distributed in the hope that it will be useful,")
+	fmt.Println("but WITHOUT ANY WARRANTY; without even the implied warranty of")
+	fmt.Println("MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the")
+	fmt.Println("GNU General Public License for more details.")
+	fmt.Println("")
+	fmt.Println("You should have received a copy of the GNU General Public License")
+	fmt.Println("along with this program.  If not, see <https://www.gnu.org/licenses/>.")
 	if len(os.Args) < 3 {
 		panic(fmt.Errorf("usage: %v [in.tune] [out.wave]", os.Args[0]))
 	}
